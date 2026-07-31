63d65ca2cb1b4b043c098202234102191eebbcc3
Raj Sharma <rajsharma331.rajvnsjob@gmail.com>
2026-07-31 12:26:32 +0530

    upgraded notification

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import axios from "axios";
 import dotenv from "dotenv";
+import useragent from "express-useragent";
+import geoip from "geoip-lite";
 
 dotenv.config();
 
@@ -9,17 +11,70 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(useragent.express());
 
 app.post("/visit", async (req, res) => {
+ 
+
   try {
+    // Client data from frontend
+    const {
+      screen,
+      language,
+      timezone,
+      referrer,
+    } = req.body;
+
+    // Get real client IP behind Render proxy
+    const ip =
+      req.headers["x-forwarded-for"]?.split(",")[0] ||
+      req.socket.remoteAddress ||
+      req.ip;
+
+    // Geo Location
+    const geo = geoip.lookup(ip);
+
+    const country = geo?.country || "Unknown";
+    const city = geo?.city || "Unknown";
+
+    // Browser & Device
+    const ua = req.useragent;
+
+    let device = "Desktop";
+
+    if (ua.isMobile) device = "Mobile";
+    if (ua.isTablet) device = "Tablet";
+
+    const browser = ua.browser;
+    const os = ua.os;
+
+    // Indian Time
+    const time = new Date().toLocaleString("en-IN", {
+      timeZone: "Asia/Kolkata",
+      dateStyle: "medium",
+      timeStyle: "medium",
+    });
+
     const message = `
 🚀 New Portfolio Visitor
 
-🌍 IP: ${req.ip}
-🖥 User Agent:
-${req.headers["user-agent"]}
+🌍 Country: ${country}
+🏙 City: ${city}
 
-🕒 Time: ${new Date().toLocaleString()}
+📱 Device: ${device}
+
+🌐 Browser: ${browser}
+💻 OS: ${os}
+
+📐 Screen: ${screen || "Unknown"}
+
+🗣 Language: ${language || "Unknown"}
+
+🕒 Timezone: ${timezone || "Unknown"}
+
+🔗 Referrer: ${referrer || "Direct"}
+
+🕒 Time: ${time}
 `;
 
     await axios.post(
